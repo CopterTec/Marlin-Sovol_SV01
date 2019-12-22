78bcdfe460e52fbb0f99b75c13f1ffc60d7a6f00
CopterTec <info@coptertec.de>
2019-12-22 22:06:32 +0100

    Change GRID_MAX_POINTS_X to 10

--- config/examples/SOVOL/SV01-SilentBoard/Configuration.h
+++ config/examples/SOVOL/SV01-SilentBoard/Configuration.h
@@ -1248,7 +1248,7 @@
   //#define MESH_EDIT_GFX_OVERLAY   // Display a graphics overlay while editing the mesh
 
   #define MESH_INSET 15              // Set Mesh bounds as an inset region of the bed
-  #define GRID_MAX_POINTS_X 3      // Don't use more than 15 points per axis, implementation limited.
+  #define GRID_MAX_POINTS_X 10       // Don't use more than 15 points per axis, implementation limited.
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   #define UBL_MESH_EDIT_MOVES_Z     // Sophisticated users prefer no movement of nozzle
