d04c4c113f3185820177002897187d01fa2891e5
CopterTec <info@coptertec.de>
2019-12-23 02:50:56 +0100

    Fix preset

--- config/examples/SOVOL/SV01-BLTouch/Configuration.h
+++ config/examples/SOVOL/SV01-BLTouch/Configuration.h
@@ -1447,10 +1447,10 @@
 #define PREHEAT_1_TEMP_BED     60
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
-#define PREHEAT_3_LABEL       "ABS"
-#define PREHEAT_3_TEMP_HOTEND 200
-#define PREHEAT_3_TEMP_BED    80
-#define PREHEAT_3_FAN_SPEED     0 // Value from 0 to 255
+#define PREHEAT_2_LABEL       "ABS"
+#define PREHEAT_2_TEMP_HOTEND 200
+#define PREHEAT_2_TEMP_BED    80
+#define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
 /**
  * Nozzle Park
