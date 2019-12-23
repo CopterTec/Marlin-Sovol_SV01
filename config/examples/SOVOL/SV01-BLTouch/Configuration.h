7b37d12eb2b08267d30a2f3ffabebb18ffd7bac6
CopterTec <info@coptertec.de>
2019-12-23 02:29:28 +0100

    Remove TPU preset

--- config/examples/SOVOL/SV01-BLTouch/Configuration.h
+++ config/examples/SOVOL/SV01-BLTouch/Configuration.h
@@ -1447,11 +1447,6 @@
 #define PREHEAT_1_TEMP_BED     60
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
-#define PREHEAT_2_LABEL       "TPU"
-#define PREHEAT_2_TEMP_HOTEND 195
-#define PREHEAT_2_TEMP_BED    60
-#define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
-
 #define PREHEAT_3_LABEL       "ABS"
 #define PREHEAT_3_TEMP_HOTEND 200
 #define PREHEAT_3_TEMP_BED    80
