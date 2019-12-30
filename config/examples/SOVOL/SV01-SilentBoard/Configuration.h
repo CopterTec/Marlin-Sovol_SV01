fb14a18dd598be476005c223a4d17029d2a4529e
CopterTec <info@coptertec.de>
2019-12-30 14:24:07 +0100

    Increase DEFAULT_MAX_ACCELERATION to Marlin default

--- config/examples/SOVOL/SV01-SilentBoard/Configuration.h
+++ config/examples/SOVOL/SV01-SilentBoard/Configuration.h
@@ -743,7 +743,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 500, 500, 100, 1000 }
+#define DEFAULT_MAX_ACCELERATION      { 500, 500, 100, 5000 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
