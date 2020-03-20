e2965f6b0af275274b86739e91487126dc34db58
CopterTec <info@coptertec.de>
2020-03-20 09:16:03 +0100

    Fix Level Bed Corners

--- config/SOVOL/SV01-SilentBoard_BLTouch/Configuration.h
+++ config/SOVOL/SV01-SilentBoard_BLTouch/Configuration.h
@@ -1329,7 +1329,7 @@
 #define LEVEL_BED_CORNERS
 
 #if ENABLED(LEVEL_BED_CORNERS)
-  #define LEVEL_CORNERS_INSET_LFRB { 37, 40, 243, 215 } // (mm) Left, Front, Right, Back insets
+  #define LEVEL_CORNERS_INSET_LFRB { 37, 40, 47, 40 } // (mm) Left, Front, Right, Back insets
   #define LEVEL_CORNERS_HEIGHT      0.0   // (mm) Z height of nozzle at leveling points
   #define LEVEL_CORNERS_Z_HOP       4.0   // (mm) Z height of nozzle between leveling points
   #define LEVEL_CENTER_TOO                // Move to the center after the last corner
